7d48604327771c2238b6b22731deaee2f0bb1606
Alexander Mamykin <109105189+InitAmkn@users.noreply.github.com>
2022-08-04 21:57:07 +0300

    Lesson1 task 1-3

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -2,11 +2,78 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Input value:");
-        int value = Convert.ToInt32(Console.ReadLine());
+        //task0();
+        //task1();
+        //task2();
+        //task3();
+        task4();
+    
+        void task0()
+        {
+                Console.WriteLine("Input value:");
+                int value = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine(value*value);
+            Console.WriteLine(value*value);   
+        }
+        void task1()//1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+        {
+                bool isMultiplication;
+                Console.WriteLine("Input first value:");
+                int value1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Input second value:");
+                int value2 = Convert.ToInt32(Console.ReadLine());
+                if(value2 == value1/value2)
+                {isMultiplication = true;}
+                else
+                {
+                    isMultiplication = false;
+                }
 
+                Console.WriteLine(isMultiplication);
+
+        }
+        void task2()//2. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+        {
+            string[] week= {"","понедельник", "вторник","среда","четверг", "пятница", "суббота","воскресенье" };
+               Console.WriteLine("Input value:");
+                int value1 = Convert.ToInt32(Console.ReadLine());
+                  Console.WriteLine(week[value1]);
+        }
+        void task3()//3. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+        {
+             Console.WriteLine("Input first value:");
+                int value1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Input second value:");
+                int value2 = Convert.ToInt32(Console.ReadLine());
+               int i=0;
+               if(value1<value2)
+                {
+                    i=value1;
+                    
+                    while(i<value2)
+                    {
+                     Console.Write($"{i} ");
+                    i++;
+                    }
+                }
+
+                if(value1>value2)
+                {
+                    i=value2;
+                    while(i<value1)
+                    {
+                     Console.Write($"{i} ");
+                      i++;
+                    }
+                }
+                Console.Write(i);
+            }
+        void task4()//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+        {
+            
+        }
+        
 
     }
+
 }

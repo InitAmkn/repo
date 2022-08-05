10f82062b0a09051245d28415bebc74546714f39
Alexander Mamykin <109105189+InitAmkn@users.noreply.github.com>
2022-08-05 22:45:49 +0300

    task1 completed

--- HomeWork_1/Task_1/Program.cs
+++ HomeWork_1/Task_1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+        
+        Console.WriteLine("Input first value:");
+        double value1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Input second value:");
+        double value2 = Convert.ToDouble(Console.ReadLine());
+
+        if(value1>value2)   Console.WriteLine($"MaxValue: {value1}");
+        if(value1<value2)   Console.WriteLine($"MaxValue: {value2}");
+        if(value1==value2)  Console.WriteLine($"{value1} = {value2}");

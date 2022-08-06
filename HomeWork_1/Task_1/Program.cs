2192da5a6d6e5541eac15f0dbecfa9def88220c6
Alexander Mamykin <109105189+InitAmkn@users.noreply.github.com>
2022-08-06 11:02:00 +0300

    task2

--- HomeWork_1/Task_1/Program.cs
+++ HomeWork_1/Task_1/Program.cs
@@ -1,9 +1,9 @@
 ﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
         
-        Console.WriteLine("Input first value:");
+        Console.WriteLine("Input first value:"); //Не допускается ввод других символов кроме цифр
         double value1 = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("Input second value:");
+        Console.WriteLine("Input second value:"); //Не допускается ввод других символов кроме цифр
         double value2 = Convert.ToDouble(Console.ReadLine());
 
         if(value1>value2)   Console.WriteLine($"MaxValue: {value1}");

a5766fbb7cc6e7172da494eaaa62e1dc776d5ade
Alexander Mamykin <109105189+InitAmkn@users.noreply.github.com>
2022-08-06 11:28:39 +0300

    task3 completed

---
+++ HomeWork_1/Task_3/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+  
+  Console.WriteLine("Введите число:"); //Не допускается ввод других символов кроме цифр
+    double value1 = Convert.ToDouble(Console.ReadLine());
+    
+     if(value1 == 0)
+    {
+        Console.WriteLine("Вы ввели 0");
+        
+    } else if(value1 % 2 == 0)
+    {
+        Console.WriteLine("Число четное");
+    } else
+    {
+         Console.WriteLine("Число нечетное");
+    }

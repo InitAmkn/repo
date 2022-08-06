360372af6d26210b019e002cb22e8b38df220afd
Alexander Mamykin <109105189+InitAmkn@users.noreply.github.com>
2022-08-06 11:57:05 +0300

    task4 completed

---
+++ HomeWork_1/Task_4/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+ Console.WriteLine("Введите число:"); //Не допускается ввод других символов кроме цифр
+    double value1 = Convert.ToDouble(Console.ReadLine());
+
+    if(value1>0)
+        { 
+            Console.WriteLine("Четные числа:"); 
+            for(int i = 2;i<=value1;i=i+2)
+                {
+                    Console.Write($"{i} ");
+                }
+        }
+    if(value1<0)
+        {
+             Console.WriteLine("Четные числа:"); 
+            for(int i = -2;i>=value1;i=i-2)
+                {
+                     Console.Write($"{i} ");
+                }
+        }
+    if(value1==0)
+        {
+            Console.WriteLine("Вы ввели 0");
+        }

780a57a29f833de8e0d3172cc9cecafa4f76c30f
Alexander Mamykin <109105189+InitAmkn@users.noreply.github.com>
2022-08-08 21:38:42 +0300

    add lesson 2 task1

--- Lesson2/Task_1/Program.cs
+++ Lesson2/Task_1/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿//1. 11. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+int randomThreeDigitNumber = new Random().Next(100,1000);
+string stringNumber = Convert.ToString(randomThreeDigitNumber);
+string stringNumberMini = "";
+for (int i = 0;i<3;i++)
+{
+   if(i!=1)stringNumberMini = stringNumberMini + stringNumber[i];
+}
+Convert.ToInt32(stringNumberMini);
+
+Console.WriteLine(randomThreeDigitNumber);
+Console.WriteLine(stringNumberMini);

441ed7934f2f08b8f92b454099698b760288b0ed
Alexander Mamykin <109105189+InitAmkn@users.noreply.github.com>
2022-08-08 21:21:41 +0300

    add lesson 2

---
+++ Lesson2/Task_0/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int maxChar(int value1, int value2)
+        {
+            int value3 = new Random().Next(value1,value2);
+            int a = value3/10;
+            int b = value3%10;
+            int c = 0;
+            if(a>b) c = a;
+            if(a<b) c = b;
+            
+            
+            Console.WriteLine(value3);
+            return c;
+        }
+
+        Console.WriteLine(maxChar(10,100));
+
+
+    }
+}

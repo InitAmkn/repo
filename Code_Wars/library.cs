2942d00938abf32359c9cf68cf7424700ec4d301
Alexander Mamykin <109105189+InitAmkn@users.noreply.github.com>
2022-08-13 21:20:54 +0300

    Cod wars add

---
+++ Code_Wars/library.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+  
+
+class library
+{
+  
+     public static int[] MoveZeroes(int[] arr) // перемещение нулей в конец массива
+        {
+            int[] arr1 = new int[arr.Length];
+            int j = 0;
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+
+                if (arr[i] != 0)
+                {
+                    arr1[j] = arr[i];
+                    j++;
+
+                  }
+            }
+            return arr1;
+        }
+
+ }
+       

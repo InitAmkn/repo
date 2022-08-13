5dd80269b543f378acf2bdae7c8f17339aef7b92
Alexander Mamykin <109105189+InitAmkn@users.noreply.github.com>
2022-08-13 13:44:38 +0300

    add homework 3 task23

---
+++ HomeWork_3/Task_23/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 23
+
+Напишите метод, который принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+ Console.Write("Введите число:");
+ int value = Convert.ToInt32(Console.ReadLine());
+ 
+    printMas(cubeTable(value));
+
+ int [] cubeTable(int value)
+    {
+        int[]mas = new int [value];
+        for(int i = 1; i<value+1; i++)
+            {
+                mas[i-1] = i*i*i;
+            }
+            return mas;
+     }  
+     
+     void printMas(int[] masX)
+    {
+        for (int i = 0; i < masX.Length; i++)
+            {
+                Console.Write($"{masX[i]} ");
+            }
+
+    }

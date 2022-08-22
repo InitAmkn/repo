230e6cdbba2b4ece1b865f576ffef34b40af75d6
Alexander Mamykin <109105189+InitAmkn@users.noreply.github.com>
2022-08-22 23:01:59 +0300

    add lesson 6 complete

--- Code_Wars/Program.cs
+++ Code_Wars/Program.cs
@@ -10,8 +10,11 @@ class Program
     static void Main(string[] args)
     {
 
-        Console.WriteLine(library.Rot13("This is my first ROT13 excercise!"));
+        //Console.WriteLine(library.Rot13("This is my first ROT13 excercise!"));
         //Console.WriteLine(library.Rot13("AaSs"));
+
+
+
     }
 
 
